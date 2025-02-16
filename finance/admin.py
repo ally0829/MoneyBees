f60766e611f78bc9b8f7deb8d77ce9d8fa0b8f6d
Chihhao Lee <2966830L@student.gla.ac.uk>
2025-02-16 14:06:30 +0000

    plus database

--- finance/admin.py
+++ finance/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Income, ExpenseCategory, Expense, MonthlyExpenseTarget
 
 # Register your models here.
+
+admin.site.register(Income)
+admin.site.register(ExpenseCategory)
+admin.site.register(Expense)
+admin.site.register(MonthlyExpenseTarget)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+# users/models.py
+class Currency(models.Model):
+    # Define a currency model with name, and symbol as an example
+    id = models.AutoField(primary_key=True)
+    currency = models.CharField(max_length=128)
+    
+    class Meta:
+        verbose_name_plural = "currency"
+
+    def __str__(self):
+        return self.currency
+
+class User(models.Model):
+    # ID is auto-generated, so no need to define it explicitly
+    id = models.AutoField(primary_key=True)
+    firstname = models.CharField(max_length=128)
+    lastname = models.CharField(max_length=128)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)  # Store hashed password later
+    currency = models.ForeignKey(Currency, on_delete=models.SET_NULL, null=True)
+    
+    class Meta:
+        verbose_name_plural = "user"
+
+    def __str__(self):
+        return f"{self.firstname} {self.lastname}"
