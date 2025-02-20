34829a0b21da8c3d026e43c4bc3759756a660134
Felix62902 <xfelixwongx@gmail.com>
2025-02-20 17:51:46 +0000

    added topbar

--- finance/views.py
+++ finance/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def profile_view(request):
-    return render(request, 'finance/profile.html')
+    return render(request, 'finance/profile.html',{"show_topbar": False})
