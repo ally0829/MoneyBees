234f3c203edf33d1b9e05f075a9771f39dab1743
Felix62902 <xfelixwongx@gmail.com>
2025-03-03 13:42:45 +0000

    updated UI and functionalities

--- finance/urls.py
+++ finance/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import profile_view
+from .views import profile_view, settings_view
 
 urlpatterns = [
     path("profile/", profile_view, name ="profile"),
+    path("settings/",settings_view, name ="settings"),
 ]
--- finance/views.py
+++ finance/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+import json
+from django.shortcuts import render
+from django.conf import settings
 
 # Create your views here.
 def profile_view(request):
     return render(request, 'finance/profile.html',{"show_topbar": False})
+
+def settings_view(request):
+     # Get the absolute path of the JSON file
+    json_path = settings.DATA_DIR / 'currencies.json'
+
+    # Load the JSON data
+    with open(json_path, 'r', encoding='utf-8') as f:
+        currencies = json.load(f)
+
+    return render(request, 'finance/settings.html',{'currencies': currencies})
