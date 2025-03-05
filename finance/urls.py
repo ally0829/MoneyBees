bbb39b07f0327a5308d2b05016eef1f1f471b5f3
Chihhao Lee <2966830L@student.gla.ac.uk>
2025-03-05 12:21:51 +0000

    modify

--- finance/urls.py
+++ finance/urls.py
@@ -3,7 +3,9 @@ from .views import (
     upcomingExpense_view, 
     add_upcoming_payment, 
     edit_upcoming_payment, 
-    delete_upcoming_payment
+    delete_upcoming_payment,
+    profile_view,
+
 )
 
 urlpatterns = [
@@ -11,4 +13,5 @@ urlpatterns = [
     path("add-payment/", add_upcoming_payment, name="add_payment"),
     path("edit-payment/<int:payment_id>/", edit_upcoming_payment, name="edit_payment"),
     path("delete-payment/<int:payment_id>/", delete_upcoming_payment, name="delete_payment"),
+    path("profile/", profile_view, name ="profile"),
 ]
